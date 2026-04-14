aa404e1ad874d68be450f2e36ca5c69eb194707c
SnowCanCode <parkerking0733@gmail.com>
2026-04-14 18:42:07 -0400

    Fix import paths for system modules
    Updated import paths for verification, promotion, and blacklist systems.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
-const verificationSystem = require('systems/verification');
-const promotionSystem = require('systems/promotions');
-const blacklistSystem = require('systems/blacklist');
+const verificationSystem = require('.systems/verification');
+const promotionSystem = require('.systems/promotions');
+const blacklistSystem = require('.systems/blacklist');
 
 const client = new Client({
   intents: [
